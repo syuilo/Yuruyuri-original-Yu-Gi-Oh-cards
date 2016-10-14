9c19cb408e6c9e5135f3704cf201552677b4a602
syuilo <syuilotan@yahoo.co.jp>
2016-10-14 15:05:17 +0900

    大天使アカリエル(C:100)の②の効果削除

--- scripts/c100.lua
+++ scripts/c100.lua
@@ -1,4 +1,9 @@
 --大天使アカリエル
+--「大天使アカリエル」は自分フィールド上に１体しか表側表示で存在できない。
+--①：このカードは戦闘では破壊されない。
+--②：フィールドのカードを破壊する魔法・罠・モンスターの効果が発動した時、このカードをリリースして発動できる。その発動を無効にし破壊する。
+--③：このカードの②の効果を適用したターンのエンドフェイズに発動できる。その効果を発動するためにリリースしたこのカードを墓地から特殊召喚する。
+--④：１ターンに１度だけ、自分の墓地に存在する「ゆるゆり」キャラクター１人を自分フィールド上に特殊召喚できる。
 function c100.initial_effect(c)
 
 	-- 1体しか存在できない
@@ -17,53 +22,43 @@ function c100.initial_effect(c)
 	e1:SetValue(c100.valcon)
 	c:RegisterEffect(e1)
 
-	-- damage reduce
+	-- Negate
 	local e2 = Effect.CreateEffect(c)
-	e2:SetDescription(aux.Stringid(100, 1))
-	e2:SetType(EFFECT_TYPE_CONTINUOUS+EFFECT_TYPE_FIELD)
+	e2:SetDescription(aux.Stringid(100, 2))
+	e2:SetCategory(CATEGORY_NEGATE+CATEGORY_DESTROY)
+	e2:SetType(EFFECT_TYPE_QUICK_O)
+	e2:SetCode(EVENT_CHAINING)
+	e2:SetProperty(EFFECT_FLAG_DAMAGE_STEP+EFFECT_FLAG_DAMAGE_CAL)
 	e2:SetRange(LOCATION_MZONE)
-	e2:SetCode(EVENT_PRE_BATTLE_DAMAGE)
 	e2:SetCondition(c100.e2condition)
+	e2:SetCost(c100.e2cost)
+	e2:SetTarget(c100.e2target)
 	e2:SetOperation(c100.e2operation)
 	c:RegisterEffect(e2)
 
-	-- Negate
+	-- Revive
 	local e3 = Effect.CreateEffect(c)
-	e3:SetDescription(aux.Stringid(100, 2))
-	e3:SetCategory(CATEGORY_NEGATE+CATEGORY_DESTROY)
-	e3:SetType(EFFECT_TYPE_QUICK_O)
-	e3:SetCode(EVENT_CHAINING)
-	e3:SetProperty(EFFECT_FLAG_DAMAGE_STEP+EFFECT_FLAG_DAMAGE_CAL)
-	e3:SetRange(LOCATION_MZONE)
-	e3:SetCondition(c100.e3condition)
-	e3:SetCost(c100.e3cost)
+	e3:SetDescription(aux.Stringid(100, 3))
+	e3:SetType(EFFECT_TYPE_TRIGGER_O+EFFECT_TYPE_FIELD)
+	e3:SetCategory(CATEGORY_SPECIAL_SUMMON)
+	e3:SetCode(EVENT_PHASE+PHASE_END)
+	e3:SetRange(LOCATION_GRAVE)
+	e3:SetCountLimit(1)
 	e3:SetTarget(c100.e3target)
 	e3:SetOperation(c100.e3operation)
 	c:RegisterEffect(e3)
 
-	-- Revive
+	-- SpecialSummon
 	local e4 = Effect.CreateEffect(c)
-	e4:SetDescription(aux.Stringid(100, 3))
-	e4:SetType(EFFECT_TYPE_TRIGGER_O+EFFECT_TYPE_FIELD)
+	e4:SetDescription(aux.Stringid(100, 4))
 	e4:SetCategory(CATEGORY_SPECIAL_SUMMON)
-	e4:SetCode(EVENT_PHASE+PHASE_END)
-	e4:SetRange(LOCATION_GRAVE)
+	e4:SetType(EFFECT_TYPE_IGNITION)
 	e4:SetCountLimit(1)
+	e4:SetProperty(EFFECT_FLAG_CARD_TARGET)
+	e4:SetRange(LOCATION_MZONE)
 	e4:SetTarget(c100.e4target)
 	e4:SetOperation(c100.e4operation)
 	c:RegisterEffect(e4)
-
-	-- SpecialSummon
-	local e5 = Effect.CreateEffect(c)
-	e5:SetDescription(aux.Stringid(100, 4))
-	e5:SetCategory(CATEGORY_SPECIAL_SUMMON)
-	e5:SetType(EFFECT_TYPE_IGNITION)
-	e5:SetCountLimit(1)
-	e5:SetProperty(EFFECT_FLAG_CARD_TARGET)
-	e5:SetRange(LOCATION_MZONE)
-	e5:SetTarget(c100.e5target)
-	e5:SetOperation(c100.e5operation)
-	c:RegisterEffect(e5)
 
 end
 
@@ -72,73 +67,62 @@ function c100.valcon(e, re, r, rp)
 	return bit.band(r, REASON_BATTLE) ~= 0
 end
 
-
-function c100.e2condition(e,tp,eg,ep,ev,re,r,rp)
-	return ep==tp and e:GetHandler():GetFlagEffect(100)==0
-end
-function c100.e2operation(e,tp,eg,ep,ev,re,r,rp)
-	if Duel.SelectEffectYesNo(tp,e:GetHandler()) then
-		Duel.ChangeBattleDamage(tp,ev/2)
-		e:GetHandler():RegisterFlagEffect(100,RESET_EVENT+0x1fe0000+RESET_PHASE+PHASE_END,0,1)
-	end
-end
-
-
-
-function c100.e3condition(e,tp,eg,ep,ev,re,r,rp)
+function c100.e2condition(e, tp, eg, ep, ev, re, r, rp)
 	if e:GetHandler():IsStatus(STATUS_BATTLE_DESTROYED) or not Duel.IsChainNegatable(ev) then return false end
-	if re:IsHasCategory(CATEGORY_NEGATE)
-		and Duel.GetChainInfo(ev-1,CHAININFO_TRIGGERING_EFFECT):IsHasType(EFFECT_TYPE_ACTIVATE) then return false end
-	local ex,tg,tc=Duel.GetOperationInfo(ev,CATEGORY_DESTROY)
-	return ex and tg~=nil and tc+tg:FilterCount(Card.IsOnField,nil)-tg:GetCount()>0
+	if re:IsHasCategory(CATEGORY_NEGATE) -- 破壊する効果を持っているか(?)
+		and Duel.GetChainInfo(ev - 1, CHAININFO_TRIGGERING_EFFECT):IsHasType(EFFECT_TYPE_ACTIVATE) then return false end
+	local ex, tg, tc = Duel.GetOperationInfo(ev, CATEGORY_DESTROY)
+	return ex and tg ~= nil and tc + tg:FilterCount(Card.IsOnField, nil) - tg:GetCount() > 0
 end
-function c100.e3cost(e,tp,eg,ep,ev,re,r,rp,chk)
-	if chk==0 then return e:GetHandler():IsReleasable() end
-	Duel.Release(e:GetHandler(),REASON_COST)
+
+function c100.e2cost(e, tp, eg, ep, ev, re, r, rp, chk)
+	if chk == 0 then return e:GetHandler():IsReleasable() end
+	Duel.Release(e:GetHandler(), REASON_COST)
 end
-function c100.e3target(e,tp,eg,ep,ev,re,r,rp,chk)
-	if chk==0 then return true end
-	Duel.SetOperationInfo(0,CATEGORY_NEGATE,eg,1,0,0)
+
+function c100.e2target(e, tp, eg, ep, ev, re, r, rp, chk)
+	if chk == 0 then return true end
+	Duel.SetOperationInfo(0, CATEGORY_NEGATE, eg, 1, 0, 0)
 	if re:GetHandler():IsDestructable() and re:GetHandler():IsRelateToEffect(re) then
-		Duel.SetOperationInfo(0,CATEGORY_DESTROY,eg,1,0,0)
+		Duel.SetOperationInfo(0, CATEGORY_DESTROY, eg, 1, 0, 0)
 	end
 end
-function c100.e3operation(e,tp,eg,ep,ev,re,r,rp)
+
+function c100.e2operation(e, tp, eg, ep, ev, re, r, rp)
 	Duel.NegateActivation(ev)
 	if re:GetHandler():IsRelateToEffect(re) then
-		Duel.Destroy(eg,REASON_EFFECT)
+		Duel.Destroy(eg, REASON_EFFECT)
 	end
-	e:GetHandler():RegisterFlagEffect(100,RESET_EVENT+0x1fe0000+RESET_PHASE+PHASE_END,0,0)
+	e:GetHandler():RegisterFlagEffect(100, RESET_EVENT + 0x1fe0000 + RESET_PHASE + PHASE_END, 0, 0)
 end
 
-function c100.e4target(e,tp,eg,ep,ev,re,r,rp,chk)
-	local c=e:GetHandler()
-	if chk==0 then return Duel.GetLocationCount(tp,LOCATION_MZONE)>0 and c:GetFlagEffect(100)>0
-		and c:IsCanBeSpecialSummoned(e,0,tp,false,false) end
-	Duel.SetOperationInfo(0,CATEGORY_SPECIAL_SUMMON,c,1,0,0)
+function c100.e3target(e, tp, eg, ep, ev, re, r, rp, chk)
+	local c = e:GetHandler()
+	if chk == 0 then return Duel.GetLocationCount(tp, LOCATION_MZONE) > 0 and c:GetFlagEffect(100) > 0
+		and c:IsCanBeSpecialSummoned(e, 0, tp, false, false) end
+	Duel.SetOperationInfo(0, CATEGORY_SPECIAL_SUMMON, c, 1, 0, 0)
 end
-function c100.e4operation(e,tp,eg,ep,ev,re,r,rp)
+
+function c100.e3operation(e, tp, eg, ep, ev, re, r, rp)
 	if e:GetHandler():IsRelateToEffect(e) then
-		Duel.SpecialSummon(e:GetHandler(),0,tp,tp,false,false,POS_FACEUP)
+		Duel.SpecialSummon(e:GetHandler(), 0, tp, tp, false, false, POS_FACEUP)
 	end
 end
 
-
-
-
-
-function c100.e5tgfilter(c, e, tp)
+function c100.e4tgfilter(c, e, tp)
 	return c:IsSetCard(0x186A0) and c:IsCanBeSpecialSummoned(e, 0, tp, false, false)
 end
-function c100.e5target(e, tp, eg, ep, ev, re, r, rp, chk, chkc)
-	if chkc then return chkc:IsLocation(LOCATION_GRAVE) and chkc:IsControler(tp) and c100.e5tgfilter(chkc,e,tp) end
-	if chk == 0 then return Duel.GetLocationCount(tp,LOCATION_MZONE)>0
-		and Duel.IsExistingTarget(c100.e5tgfilter,tp,LOCATION_GRAVE,0,1,nil,e,tp) end
-	Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_SPSUMMON)
-	local g=Duel.SelectTarget(tp,c100.e5tgfilter,tp,LOCATION_GRAVE,0,1,1,nil,e,tp)
-	Duel.SetOperationInfo(0,CATEGORY_SPECIAL_SUMMON,g,1,0,0)
+
+function c100.e4target(e, tp, eg, ep, ev, re, r, rp, chk, chkc)
+	if chkc then return chkc:IsLocation(LOCATION_GRAVE) and chkc:IsControler(tp) and c100.e4tgfilter(chkc, e, tp) end
+	if chk == 0 then return Duel.GetLocationCount(tp, LOCATION_MZONE) > 0
+		and Duel.IsExistingTarget(c100.e4tgfilter, tp, LOCATION_GRAVE, 0, 1, nil, e, tp) end
+	Duel.Hint(HINT_SELECTMSG, tp, HINTMSG_SPSUMMON)
+	local g = Duel.SelectTarget(tp, c100.e4tgfilter, tp, LOCATION_GRAVE, 0, 1, 1, nil, e, tp)
+	Duel.SetOperationInfo(0, CATEGORY_SPECIAL_SUMMON, g, 1, 0, 0)
 end
-function c100.e5operation(e, tp, eg, ep, ev, re, r, rp)
+
+function c100.e4operation(e, tp, eg, ep, ev, re, r, rp)
 	if Duel.GetLocationCount(tp, LOCATION_MZONE) <= 0 then return end
 	local tc = Duel.GetFirstTarget()
 	if tc:IsRelateToEffect(e) then
